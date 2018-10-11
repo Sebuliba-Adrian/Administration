0289eee912524bb4b3e8da2954bca4ae0b2ded5b
adrian <adrian.sebuliba1@gmail.com>
2018-10-11 21:09:29 +0300

    chore(setup-project): setup project stucture and development environment

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    parser: "babel-eslint",
+    plugins: ["react", "prettier", "jest"],
+    env: {
+        browser: true
+    },
+    extends: [
+        "airbnb",
+        "plugin:prettier/recommended",
+        "prettier/react",
+        "prettier/standard",
+        "plugin:jest/recommended"
+    ],
+    rules: {
+        indent: 0,
+
+        "prettier/prettier": "error",
+
+        "react/jsx-indent": 0,
+        "react/jsx-indent-props": 0,
+        "react/jsx-filename-extension": 0
+    }
+};
